0fda735e6cab6c891bf2240f8ab7f74826c8c15d
rene huber <huembi@gmail.com>
2015-09-02 13:49:55 +0200

    search.html works a bit

--- dashboard/restserver.js
+++ dashboard/restserver.js
@@ -29,6 +29,7 @@ server.get("/alive",  function (req, res, next) {
   return;
 });
 
+// Return ALL products
 server.get("/products", function (req, res, next) {
     console.log("request products ");
     db.products.find(function (err, products) {
@@ -40,6 +41,57 @@ server.get("/products", function (req, res, next) {
     console.log("request products returns");
     return next();
 });
+
+
+// return the last N products from stragtegy
+// usage: http://localhost:3000/products/100/strategy/no-cache
+server.get("/products/:n/strategy/:strategy", function (req, res, next) {
+	var n = req.params.n;
+	var strategy = req.params.strategy;
+	console.log("request "+n+" products  ");
+	
+	db.products.find({ "strategy":strategy }).sort({_id: -1}).limit(n, function (err, products) {
+        res.writeHead(200, {
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+        res.end(JSON.stringify(products));
+    });
+    return next();
+});
+
+	
+		
+	// TRY TO GET ENTRIES STARTING WITH :ID, BUT NOT WORKING YET...
+server.get("/productfrom/:id", function (req, res, next) {
+	var id = req.params.id;
+	console.log("request  products  from "+id);
+	db.products.find({ "strategy":"no-cache" }).sort({_id: -1}).limit(1000, function (err, products) {
+		//	var newestEntry = parseInt(products[0]._id, 16);
+//	var requestEntry = parseInt(req.params.id, 16);
+//	var x = newestEntry - requestEntry;
+//	console.log("request x = lastId - id ="+ x +" = "+newestEntry+" - "+requestEntry);
+//		db.     col.find({_id: {$gt: {ObjectId("50911c4709913b2c643f1216")}}});
+//	db.products.find({ "strategy":"no-cache" }).sort({_id: -1}).limit(10, function (err, products) {
+	//db.products.find(function (err, products) {
+//	   db.products.find( { _id: { $gt: requestID } }  , function(err, products) {
+//		  db.products.find(  function(err, products) {
+		  res.writeHead(200, {
+		      'Content-Type': 'application/json; charset=utf-8'
+		  });
+		  for(var x in products){
+			    console.log(": "+products[x]._id);
+			    if (products[x]._id == requestID) {
+			    	console("ABORT "+x	)
+			    }
+		  }
+		  
+		  res.end(JSON.stringify(products));
+	});
+	console.log("request products returns");
+	return next();
+	
+});
+
 
 server.get('/product/:id', function (req, res, next) {
     db.products.findOne({
@@ -59,19 +111,23 @@ server.post('/product', function (req, res, next) {
     var product = req.params;
     db.products.save(product,
         function (err, data) {
-            res.writeHead(200, {
-                'Content-Type': 'application/json; charset=utf-8'
-            });
-            res.end(JSON.stringify(data));
+    	   console.log("store /product  "+JSON.stringify(data));
+           res.writeHead(200, {'Content-Type': 'text/html'});
+           res.end();
+//            res.writeHead(200, {
+//                'Content-Type': 'application/json; charset=utf-8'
+//            });
+//            res.end();
+            return;
         });
     
     // "strategy":"no-cache"
     // FIXME: get count of all documents of the given strategx....o
    // console.log("stored   "+db.products.find({"strategy":"no-cache"}).count());
-    return next();
+    return "";
 });
 
-server.put('/product/:id', function (req, res, next) {
+server.post('/product/:id', function (req, res, next) {
    console.log("store /product/:id  ");
     // get the existing product
     db.products.findOne({
@@ -98,7 +154,7 @@ server.put('/product/:id', function (req, res, next) {
             res.end(JSON.stringify(data));
         });
     });
-    return next();
+    return "";
 });
 
 // deletes the given id
